6d61e6e921ea7a17d0348cfce26e1a817e53e64e
franklin <franklin.razafinandrasana@pulse.mg>
2024-12-26 15:59:35 +0300

    build back end

---
+++ serveur/dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const camRoute_1 = __importDefault(require("./routes/camRoute"));
+const adminRoute_1 = __importDefault(require("./routes/adminRoute"));
+const app = (0, express_1.default)();
+// Middleware pour parser les requêtes JSON
+app.use(body_parser_1.default.json());
+// Configuration CORS pour toutes les requêtes
+app.use((0, cors_1.default)());
+// Utilisation des routes définies dans userRoutes
+app.use('/api/v1/', camRoute_1.default);
+app.use('/api/v1/', adminRoute_1.default);
+// Démarrer le serveur
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
